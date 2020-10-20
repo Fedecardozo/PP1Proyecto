14f2489bd9e8d000c1f96990630daf36882d947d
FedericoCardozo <fedecardozo.a@gmail.com>
2020-10-19 22:38:09 -0300

    Pongo el boton para hacer pedidos

--- Version2/js/script.js
+++ Version2/js/script.js
@@ -3,19 +3,30 @@ function formarX(){
     var span1 = document.getElementById("linea1");
     var span2 = document.getElementById("linea2");
     var span3 = document.getElementById("linea3");
-
+    var nav = document.getElementById("mostrar-nav");
+    
+    
     if(span1.className == 'derecho1'){
-
+        
         span1.className = 'cruzado1';
         span2.className = 'cruzado2';
         span3.className = 'cruzado3';
-
+        
     }else{
-
+        
         span1.className = 'derecho1';
         span2.className = 'derecho2';
         span3.className = 'derecho3';
-
+        
     }
-
+    
+    if(nav.hidden == true){
+        
+        nav.hidden = false;
+        
+    }else{
+        
+        nav.hidden = true;
+        
+    }
 }
